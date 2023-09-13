61575cd0443622be53cde45a2130ab18e49504ca
jangjiyoung <dl50999@naver.com>
2023-09-13 09:28:11 +0900

    SCM수입검사 DB연결

--- lib/src/scm_admin/scm_check_controller.dart
+++ lib/src/scm_admin/scm_check_controller.dart
@@ -1,46 +1,50 @@
-
 import 'package:hnde_pda/src/scm_admin/scm_check_model.dart';
 import 'package:flutter/material.dart';
 
-
 class ScmCheckController {
-
   ScmCheckModel model = ScmCheckModel();
 
-  Future<void> pageLoad()async{
+  Future<void> pageLoad() async {
     await model.pageLoad();
   }
 
-  Future<void> setKeyboardClick(bool bo)async{
+  Future<void> setKeyboardClick(bool bo) async {
     await model.setKeyboardClick(bo);
   }
 
-  dynamic textFocusListner(BuildContext context, void Function()? state){
-    return model.textFocusListner(context,state);
+  dynamic textFocusListner(BuildContext context, void Function()? state) {
+    return model.textFocusListner(context, state);
   }
 
-  dynamic barcodeFocusListner(BuildContext context){
+  dynamic barcodeFocusListner(BuildContext context) {
     return model.barcodeFocusListner(context);
   }
 
-  FocusNode getTextNode(){
-    return  model.getTextNode();
+  FocusNode getTextNode() {
+    return model.getTextNode();
   }
 
-  FocusNode getBarcodeNode(){
+  FocusNode getBarcodeNode() {
     return model.getBarcodeNode();
   }
 
-  Future<void>setFocus(BuildContext context)async{
+  Future<void> setFocus(BuildContext context) async {
     model.setFocus(context);
   }
 
-  Color setKeyboardColor(){
+  Color setKeyboardColor() {
     return model.setKeyboardColor();
   }
 
-  Future<void> scanBarcode(String barcode)async{
-    model.scanBarcode(barcode);
+  Future<void> scanBarcode(String barcode) async {
+    await model.scanBarcode(barcode);
   }
 
+  String getPsuNb() {
+    return model.getPsuNb();
+  }
+
+  String getTrNm() {
+    return model.getTrNm();
+  }
 }
